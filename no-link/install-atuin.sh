0fc78d2bc04ded5ce495eabf912fd787cc540201
ajn142 <5253718+ajn142@users.noreply.github.com>
2025-03-14 04:40:44 -0500

    Fixed variable and testing import changes

--- no-link/install-atuin.sh
+++ no-link/install-atuin.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-$CWD=$"(pwd)"
+CWD="$(pwd)"
 
 # verify curl is installed
 ! [[ -f $(command -v curl) ]] && echo "curl not installed, prompting to install" && sudo apt install curl
@@ -8,11 +8,11 @@ $CWD=$"(pwd)"
 # install atuin to standard location (~/.local/share/atuin)
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/atuinsh/atuin/releases/latest/download/atuin-installer.sh | sh
 
-# import history before reloading bash and making atuin active
-atuin import auto
-
 # reload bash with atuin in place
 exec bash
+
+# import history
+atuin import auto
 
 # return to original directory
 cd $CWD
